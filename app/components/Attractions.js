b6ed28882e855ac0efe862a085ab779c6b4183b6
BHONEKHANT <bhonekhant.t67@rsu.ac.th>
2025-09-23 00:36:32 +0700

    Updated Title in Attractions.js

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -48,8 +48,9 @@ export default function Attractions() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+        Bhone Khant Thu 
       </h1>
+      <h2>6708219</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {attractions.map((attraction) => (
